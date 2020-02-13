4aeed26bd33621c1bc1141c32f461ec68fe70329
kadir yorulmaz <kadiryorulmaz1@hotmail.com>
2020-02-13 16:04:57 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TodoList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var name = Console.ReadLine();
+            Console.WriteLine("Hello you wrote: " + name);
+        }
+    }
+}
---
+++ TodoList.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TodoList
+{
+    class TodoList
+    {
+        //TODO
+        //PreAdd TodoTask
+        //List Tasks
+        //Create new task
+        //Delete Existing Task
+        //Edit Existing Task
+
+        List<TodoList> todolistArray = new List<TodoList>();
+
+        [MinLength(1), MaxLength(3)]
+        public string TaskId { get; set; }
+        public string TaskTitle { get; set; }
+        public string TaskDescription { get; set; }
+        public DateTime TaskCreatedDate { get; set; }
+
+        public TodoList(string id, string taskTitle, string taskDescription, DateTime taskCreatedDate)
+        {
+            id = TaskId;
+            taskTitle = TaskTitle;
+            taskCreatedDate = TaskCreatedDate;
+            taskCreatedDate = TaskCreatedDate;
+        }   
+
+        public void addTask(string taskTitle, string taskDescription)
+        {
+            var tempTaskID = "AK3";
+            var task = new TodoList(tempTaskID, taskTitle, taskDescription, DateGenerator()); ;
+            todolistArray.Add(task);
+        }
+
+        public void deleteTask()
+        {
+            
+        }
+
+        public void editTask()
+        {
+
+        }
+        
+        public DateTime DateGenerator()
+        {
+            DateTime localDateTime = DateTime.Now;
+            Console.WriteLine(localDateTime);
+            return localDateTime;
+        }
+
+    }
+}
