133d85f18ab80c3087d9fb679130d48431b6022d
kadir yorulmaz <kadiryorulmaz1@hotmail.com>
2020-02-13 20:25:48 +0100

    show task with loop

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 
+
 namespace TodoList
 {
     class Program
     {
         static void Main(string[] args)
         {
+            TodoList.preAddTasks();
+
             Console.WriteLine("Hello World!");
+            Console.WriteLine(TodoList.showTask());
+
 
             var name = Console.ReadLine();
             Console.WriteLine("Hello you wrote: " + name);
--- TodoList.cs
+++ TodoList.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Text;
 
 namespace TodoList
@@ -15,7 +16,7 @@ namespace TodoList
         //Delete Existing Task
         //Edit Existing Task
 
-        List<TodoList> todolistArray = new List<TodoList>();
+        public static List<TodoList> todolistArray = new List<TodoList>();
 
         [MinLength(1), MaxLength(3)]
         public string TaskId { get; set; }
@@ -25,10 +26,10 @@ namespace TodoList
 
         public TodoList(string id, string taskTitle, string taskDescription, DateTime taskCreatedDate)
         {
-            id = TaskId;
-            taskTitle = TaskTitle;
-            taskCreatedDate = TaskCreatedDate;
-            taskCreatedDate = TaskCreatedDate;
+            TaskId = id;
+            TaskTitle = taskTitle;
+            TaskDescription = taskDescription;
+            TaskCreatedDate = taskCreatedDate;
         }   
 
         public void addTask(string taskTitle, string taskDescription)
@@ -48,12 +49,56 @@ namespace TodoList
 
         }
         
-        public DateTime DateGenerator()
+        public static List<TodoList> showTask()
+        {
+            Console.WriteLine("Hello What am I");
+            Console.WriteLine(todolistArray);
+            Console.WriteLine("Hello What am I");
+
+            //todolistArray.FindAll( x => x.TaskId);
+            //return TodoList.Select(x => x);
+            Console.WriteLine("TaskId | TaskTitle | TaskDescription | TaskCreatedDate");
+
+            foreach (TodoList t in todolistArray)
+            {
+                Console.WriteLine(t.TaskId + ' ' + t.TaskTitle + ' ' + t.TaskDescription + ' ' + t.TaskCreatedDate);
+            }
+
+            //for (int i = 0; i < todolistArray.Count; i++)
+            //{
+               
+            //    Console.WriteLine(todolistArray[i].TaskId + ' ' + todolistArray[i].TaskTitle + ' ' + todolistArray[i].TaskDescription + ' ' + todolistArray[i].TaskCreatedDate);
+            //}
+            return todolistArray.ToList();
+        }
+
+        public static DateTime DateGenerator()
         {
             DateTime localDateTime = DateTime.Now;
             Console.WriteLine(localDateTime);
             return localDateTime;
         }
 
+        public static void preAddTasks()
+        {
+            //Pre-fill the list with tasks
+            
+            var task1 = new TodoList("AKY", "Wake up", "Wake up and eat breakfast", DateGenerator());
+            var task2 = new TodoList("AKI", "Cut Hair", "Cut hair at the barber", DateGenerator());
+            var task3 = new TodoList("AKO", "Go to Meeting", "Meeting with the team", DateGenerator());
+            var task4 = new TodoList("AKY", "Cross fit", "Crossfit 8:00-9:00", DateGenerator());
+            var task5 = new TodoList("ENO", "Dinner", "Eat fastfood", DateGenerator());
+            todolistArray.Add(task1);
+            todolistArray.Add(task2);
+            todolistArray.Add(task3);
+            todolistArray.Add(task4);
+            todolistArray.Add(task5);
+
+        }
+
+     
+
+
     }
+
 }
